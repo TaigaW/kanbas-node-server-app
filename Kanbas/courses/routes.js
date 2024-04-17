afea527613ea87bd4b2954c774c0e0cd06a6c023
Taiga W <taigaw@Taigas-MacBook-Pro.local>
2024-04-17 09:27:22 -0400

    a6

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,14 +1,14 @@
 import Database from "../Database/index.js";
 export default function CourseRoutes(app) {
-    app.get("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        const course = Database.courses
-          .find((c) => c._id === id);
-        if (!course) {
-          res.status(404).send("Course not found");
-          return;
-        }
-        res.send(course);
+    app.put("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        const moduleIndex = db.modules.findIndex(
+          (m) => m._id === mid);
+        db.modules[moduleIndex] = {
+          ...db.modules[moduleIndex],
+          ...req.body
+        };
+        res.sendStatus(204);
       });
     
     app.delete("/api/courses/:id", (req, res) => {
