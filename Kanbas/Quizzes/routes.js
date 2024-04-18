a20b880fdae7343dd1b9170c06fb2189383d4cc0
Taiga W <taigaw@Taigas-MacBook-Pro.local>
2024-04-18 18:08:17 -0400

    more changes

--- Kanbas/Quizzes/routes.js
+++ Kanbas/Quizzes/routes.js
@@ -1,44 +1,65 @@
-// export default function QuizRoutes(app) {
+// import db from "../Database/index.js";
+// function QuizRoutes(app) {
+//   app.get("/api/quizzes/:quizName", (req, res) => {
+//     const { quizName } = req.params;
+//     const quiz = db.quizzes
+//       .filter((q) => q.quizName === quizName);
+//     res.send(quiz);
+//   });
 
+//   app.post("/api/quizzes/:quizName", (req, res) => {
+//     const { quizName } = req.params;
+//     const newQuiz = {
+//         ...req.body,
+//         quizName: quizName,
+//         _id: new Date().getTime().toString(),
+//     };
+//     db.modules.push(newQuiz);
+//     res.send(newQuiz);
+//   });
 
-//     const saveQuiz = async (req, res) => {
-//         try {
-//             const quizData = req.body;
-//             // Assuming `Quiz` is a Mongoose model
-//             const quiz = new Quiz(quizData);
-//             await quiz.save();
-//             res.status(201).json(quiz); // Send back the saved quiz data, including its ID
-//         } catch (error) {
-//            res.status(500).json({ message: error.message });
-//         }
-//     }
-
-
-
-//     app.post('/api/quizzes', saveQuiz);
 // }
-
-
+// export default QuizRoutes;
 
 import db from "../Database/index.js";
 function QuizRoutes(app) {
-  app.get("/api/quizzes/:quizName", (req, res) => {
-    const { quizName } = req.params;
-    const quiz = db.quizzes
-      .filter((q) => q.quizName === quizName);
-    res.send(quiz);
+  app.get("/api/courses/:cid/quizzes", (req, res) => {
+    const { cid } = req.params;
+    const quizzes = db.quizzes
+      .filter((quiz) => quiz.course === cid);
+    res.send(quizzes);
   });
 
-  app.post("/api/quizzes/:quizName", (req, res) => {
-    const { quizName } = req.params;
+  app.post("/api/courses/:cid/quizzes", (req, res) => {
+    const { cid } = req.params;
     const newQuiz = {
-        ...req.body,
-        quizName: quizName,
-        _id: new Date().getTime().toString(),
+      ...req.body,
+      course: cid,
+      _id: new Date().getTime().toString(),
     };
-    db.modules.push(newQuiz);
+    db.quizzes.push(newQuiz);
     res.send(newQuiz);
   });
+
+  app.delete("/api/quizzes/:qid", (req, res) => {
+    const { qid } = req.params;
+    db.quizzes = db.quizzes.filter((quiz) => quiz._id !== qid);
+    res.sendStatus(200);
+  });
+
+  app.put("/api/quizzes/:qid", (req, res) => {
+    const { qid } = req.params;
+    const quizIndex = db.quizzes.findIndex(
+      (q) => q._id === qid);
+    db.quizzes[quizIndex] = {
+      ...db.quizzes[quizIndex],
+      ...req.body
+    };
+    res.sendStatus(204);
+});
+
+  
+
 
 }
 export default QuizRoutes;
