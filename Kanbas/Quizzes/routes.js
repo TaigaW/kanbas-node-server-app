43945aa02b6b19b8a9a6ff80c86c23d4a97b3e83
Taiga W <taigaw@Taigas-MacBook-Pro.local>
2024-04-19 18:39:03 -0400

    more stuff

--- Kanbas/Quizzes/routes.js
+++ Kanbas/Quizzes/routes.js
@@ -1,26 +1,3 @@
-// import db from "../Database/index.js";
-// function QuizRoutes(app) {
-//   app.get("/api/quizzes/:quizName", (req, res) => {
-//     const { quizName } = req.params;
-//     const quiz = db.quizzes
-//       .filter((q) => q.quizName === quizName);
-//     res.send(quiz);
-//   });
-
-//   app.post("/api/quizzes/:quizName", (req, res) => {
-//     const { quizName } = req.params;
-//     const newQuiz = {
-//         ...req.body,
-//         quizName: quizName,
-//         _id: new Date().getTime().toString(),
-//     };
-//     db.modules.push(newQuiz);
-//     res.send(newQuiz);
-//   });
-
-// }
-// export default QuizRoutes;
-
 import db from "../Database/index.js";
 function QuizRoutes(app) {
   app.get("/api/courses/:cid/quizzes", (req, res) => {
@@ -29,6 +6,12 @@ function QuizRoutes(app) {
       .filter((quiz) => quiz.course === cid);
     res.send(quizzes);
   });
+
+  app.get("api/quizzes/:qid/quizInfo", (req, res) => {
+    const { qid } = req.params;
+    const quiz = db.quizzes.filter((quiz) => quiz._id === qid);
+    res.send(quiz)
+  })
 
   app.post("/api/courses/:cid/quizzes", (req, res) => {
     const { cid } = req.params;
@@ -56,10 +39,6 @@ function QuizRoutes(app) {
       ...req.body
     };
     res.sendStatus(204);
-});
-
-  
-
-
+  });
 }
 export default QuizRoutes;
